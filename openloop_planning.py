ece6cc6b9631971aab4520f92b6cbc9b9dace516
Khushal Brahmbhatt <khushalbr@gmail.com>
2021-03-08 23:39:55 +0300

    added goal states loop and lattice planner pipeline

--- openloop_planning.py
+++ openloop_planning.py
@@ -1,10 +1,11 @@
 import sys
-# sys.path.append('/home/khushal/PycharmProjects/Lyft-Motion-Planning/l5kit/l5kit')
-sys.path.append('/Users/nicole/OSU/Lyft-Motion-Planning/l5kit/l5kit')
+sys.path.append('/home/khushal/PycharmProjects/Lyft-Motion-Planning/l5kit/l5kit')
+# sys.path.append('/Users/nicole/OSU/Lyft-Motion-Planning/l5kit/l5kit')
 
 from tempfile import gettempdir
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
@@ -22,20 +23,20 @@ from l5kit.random import GaussianRandomGenerator
 
 from extract_map import extract_map
 from path_generator import PathGenerator
-import os
+import pdb
 
 # Prepare data path and load cfg
 # set env variable for data
-# os.environ["L5KIT_DATA_FOLDER"] = "prediction-dataset/"
-os.environ["L5KIT_DATA_FOLDER"] = '/Users/nicole/OSU/Lyft-Motion-Planning/prediction-dataset'
+os.environ["L5KIT_DATA_FOLDER"] = "prediction-dataset/"
+# os.environ["L5KIT_DATA_FOLDER"] = '/Users/nicole/OSU/Lyft-Motion-Planning/prediction-dataset'
 dm = LocalDataManager(None)
 # get config
-# cfg = load_config_data("prediction-dataset/config.yaml")
-cfg = load_config_data("/Users/nicole/OSU/Lyft-Motion-Planning/prediction-dataset/config.yaml")
+cfg = load_config_data("prediction-dataset/config.yaml")
+# cfg = load_config_data("/Users/nicole/OSU/Lyft-Motion-Planning/prediction-dataset/config.yaml")
 
 # Load the model
-# model_path = "prediction-dataset/planning_model_20201208.pt"
-model_path = '/Users/nicole/OSU/l5kit/pre-trained-models/planning_model_20201208.pt'
+model_path = "prediction-dataset/planning_model_20201208.pt"
+# model_path = '/Users/nicole/OSU/l5kit/pre-trained-models/planning_model_20201208.pt'
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = torch.load(model_path).to(device)
 model = model.eval()
@@ -48,6 +49,65 @@ eval_dataset = EgoDataset(cfg, eval_zarr, rasterizer)
 eval_dataloader = DataLoader(eval_dataset, shuffle=eval_cfg["shuffle"], batch_size=eval_cfg["batch_size"],
                              num_workers=eval_cfg["num_workers"])
 print(eval_dataset)
+
+
+def get_goal_states(x, y, theta, num_paths=7, offset=1.5):
+    """
+    Function to get list of laterally offset goal states.
+    :param x: x-coordinate of goal position
+    :param y: y-coordinate of goal position
+    :param theta: heading of goal position (in radians in local frame)
+    :param num_paths: no. of lateral goal positions to generate paths to
+    :param offset: lateral offset to place goal positions; can be width of the ego vehicle
+    :return: list of laterally offset goal states; each goal state is a list of the form [x, y, theta]
+    """
+    goal_state_set = []
+    for i in range(num_paths):
+        goal_offset = (i - num_paths // 2) * offset  # how much to offset goal at ith position by
+        x_offset = offset * np.cos(theta + np.pi/2)  # x-axis projection
+        y_offset = offset * np.sin(theta + np.pi/2)  # y-axis projection
+        goal_state_set.append([x + x_offset, y + y_offset, theta])
+
+    return goal_state_set
+
+
+def lattice_planner(data, rasterizer):
+    """
+    Function to generate best trajectory using a lattice planner.
+    :param data: a data sample from the dataloader
+    :param rasterizer: the l5kit rasterizer
+    :return: dictionary containing positions and headings along trajectory obtained using lattice planner
+    """
+
+    # get initialization parameters from the data
+    lane_map, start_position, end_position, start_heading, end_heading = extract_map(data, rasterizer)
+    start_x = start_position[0]
+    start_y = start_position[1]
+    start_theta = start_heading[0]
+    start_curvature = 0
+    goal_x = end_position[0]
+    goal_y = end_position[1]
+    goal_theta = end_heading[0]
+    goal_curvature = 0
+
+    # get list of goal states for lattice using lateral offsets
+    goal_state_set = get_goal_states(goal_x, goal_y, goal_theta)
+
+    # get optimized paths over all goal states
+    paths = []
+    for goal_state in goal_state_set:
+        goal_x = goal_state[0]
+        goal_y = goal_state[1]
+        goal_theta = goal_state[2]
+        pg = PathGenerator(start_x, start_y, start_theta, start_curvature,
+                           goal_x, goal_y, goal_theta, goal_curvature,
+                           alpha=10, beta=10, gamma=10, kmax=0.5)
+        paths.append(pg.path)
+
+    # TODO: run collision checking and get path with best score
+
+    return 1
+
 
 # Evaluation loop
 position_preds_dl = []
@@ -63,47 +123,58 @@ torch.set_grad_enabled(False)
 
 # for idx_data, data_ in enumerate(tqdm(eval_dataloader)):
 #     data = {k: v.to(device) for k, v in data_.items()}
+#
+#     # Deep learning results
 #     result_dl = model(data)
 #     position_preds_dl.append(result_dl["positions"].detach().cpu().numpy())
 #     yaw_preds_dl.append(result_dl["yaws"].detach().cpu().numpy())
+#
+#     # Lattice planner results
+#     result_lp = lattice_planner(data, rasterizer)
+#     position_preds_lp.append(result_lp)
+#     yaw_preds_lp.append(result_lp)
+#
+#     # Ground truth
+#     position_gts.append(data["target_positions"].detach().cpu().numpy())
+#     yaw_gts.append(data["target_yaws"].detach().cpu().numpy())
+#     if idx_data == 10:
+#         break
+
 idx_data = 0
 for frame_number in range(0, len(eval_dataset), len(eval_dataset) // 20):
-    data = eval_dataloader.dataset[frame_number]
+    data = eval_dataloader.dataset[frame_number]  # ndarrays on cpu for lattice planner
+    data_batch = default_collate([data])  # tensors on cuda/cpu device for deep learning model
 
-    data_batch = default_collate([data])
-
+    # Deep learning results
     result_dl = model(data_batch)
-    predicted_positions = result_dl["positions"].detach().cpu().numpy().squeeze()
-    print('dl predicted positions')
-    print(predicted_positions)
-    yaw_positions = result_dl["yaws"].detach().cpu().numpy().squeeze()
-    
-    position_preds_dl.append(predicted_positions)
-    yaw_preds_dl.append(yaw_positions)
+    position_preds_dl.append(result_dl["positions"].detach().cpu().numpy().squeeze())  # TODO: check shape consistencies if squeeze is required
+    yaw_preds_dl.append(result_dl["yaws"].detach().cpu().numpy().squeeze())
 
-    # TODO: Lattice Planner results
+    # Lattice planner results
     print('lattice plan for frame number {f}'.format(f=frame_number))
-    our_map, start_position, end_position, start_heading, end_heading = extract_map(data, rasterizer)
-    start_x = start_position[0]
-    start_y = start_position[1]
-    start_theta = start_heading
-    start_curvature = 0
-    goal_x = end_position[0]
-    goal_y = end_position[1]
-    goal_theta = end_heading
-    goal_curvature = 1
-    pg = PathGenerator(start_x, start_y, start_theta, start_curvature, 
-        goal_x, goal_y, goal_theta, goal_curvature,
-        alpha=10, beta=10, gamma=10, kmax=0.5)
-    print(pg.path)
-    # position_preds_lp.append(result_lp["positions"].detach().cpu().numpy())
-    # yaw_preds_lp.append(result_lp["yaws"].detach().cpu().numpy())
+    result_lp = lattice_planner(data, rasterizer)  # TODO: lattice planner path
+    position_preds_lp.append(result_lp)  # TODO: set to list of 50 positions
+    yaw_preds_lp.append(result_lp)  # TODO: set to list of 50 headings
+#     our_map, start_position, end_position, start_heading, end_heading = extract_map(data, rasterizer)
+#     start_x = start_position[0]
+#     start_y = start_position[1]
+#     start_theta = start_heading
+#     start_curvature = 0
+#     goal_x = end_position[0]
+#     goal_y = end_position[1]
+#     goal_theta = end_heading
+#     goal_curvature = 1
+#     pg = PathGenerator(start_x, start_y, start_theta, start_curvature,
+#         goal_x, goal_y, goal_theta, goal_curvature,
+#         alpha=10, beta=10, gamma=10, kmax=0.5)
+#     print(pg.path)
 
-    # position_gts.append(data["target_positions"].detach().cpu().numpy())
-    # yaw_gts.append(data["target_yaws"].detach().cpu().numpy())
-    position_gts.append(data["target_positions"])
-    yaw_gts.append(data["target_yaws"])
-    
+    # Ground truth
+    position_gts.append(data["target_positions"].detach().cpu().numpy())  # TODO: check shape consistencies if squeeze is required
+    yaw_gts.append(data["target_yaws"].detach().cpu().numpy())
+#     position_gts.append(data["target_positions"])
+#     yaw_gts.append(data["target_yaws"])
+
     if idx_data == 10:
         break
     idx_data += 1
