dacf2f09452182b48b0f503aa8ffa230e1bc2be1
Khushal Brahmbhatt <khushalbr@gmail.com>
2021-03-09 10:38:54 +0300

    path optimizer using coursera solution

--- openloop_planning.py
+++ openloop_planning.py
@@ -23,6 +23,7 @@ from l5kit.random import GaussianRandomGenerator
 
 from extract_map import extract_map
 from path_generator import PathGenerator
+from path_optimizer import PathOptimizer
 import pdb
 
 # Prepare data path and load cfg
@@ -99,14 +100,19 @@ def lattice_planner(data, rasterizer):
         goal_x = goal_state[0]
         goal_y = goal_state[1]
         goal_theta = goal_state[2]
-        pg = PathGenerator(start_x, start_y, start_theta, start_curvature,
-                           goal_x, goal_y, goal_theta, goal_curvature,
-                           alpha=10, beta=10, gamma=10, kmax=0.5)
-        paths.append(pg.path)
+        # pg = PathGenerator(start_x, start_y, start_theta, start_curvature,
+        #                    goal_x, goal_y, goal_theta, goal_curvature,
+        #                    alpha=10, beta=10, gamma=10, kmax=0.5)
+        # paths.append(pg.path)
+        pg = PathOptimizer(start_x, start_y, start_theta, goal_x, goal_y, goal_theta)
+        path = pg.optimize_spiral(num_samples=data['target_positions'].shape[0] + 1)  # add 1 to include start pos
+        paths.append(path)
 
-    # TODO: run collision checking and get path with best score
+    path = paths[3]  # TODO: run collision checking and get path with best score
+    positions = np.array(path[:2]).T  # shape: nx2
+    headings = path[2][1:]  # shape: nx1; first element is start position
 
-    return 1
+    return positions, headings
 
 
 # Evaluation loop
@@ -126,13 +132,13 @@ torch.set_grad_enabled(False)
 #
 #     # Deep learning results
 #     result_dl = model(data)
-#     position_preds_dl.append(result_dl["positions"].detach().cpu().numpy())
-#     yaw_preds_dl.append(result_dl["yaws"].detach().cpu().numpy())
+#     position_preds_dl.append(result_dl["positions"].detach().cpu().numpy())  # shape: [batch, future_num_frames, 2]
+#     yaw_preds_dl.append(result_dl["yaws"].detach().cpu().numpy())  # shape: [batch, future_num_frames, 1]
 #
 #     # Lattice planner results
-#     result_lp = lattice_planner(data, rasterizer)
-#     position_preds_lp.append(result_lp)
-#     yaw_preds_lp.append(result_lp)
+#     # postions_lp, yaws_lp = lattice_planner(data, rasterizer)
+#     # position_preds_lp.append(postions_lp)
+#     # yaw_preds_lp.append(yaws_lp)
 #
 #     # Ground truth
 #     position_gts.append(data["target_positions"].detach().cpu().numpy())
@@ -147,61 +153,47 @@ for frame_number in range(0, len(eval_dataset), len(eval_dataset) // 20):
 
     # Deep learning results
     result_dl = model(data_batch)
-    position_preds_dl.append(result_dl["positions"].detach().cpu().numpy().squeeze())  # TODO: check shape consistencies if squeeze is required
-    yaw_preds_dl.append(result_dl["yaws"].detach().cpu().numpy().squeeze())
+    position_preds_dl.append(result_dl["positions"].detach().cpu().numpy())
+    yaw_preds_dl.append(result_dl["yaws"].detach().cpu().numpy())
 
     # Lattice planner results
     print('lattice plan for frame number {f}'.format(f=frame_number))
-    result_lp = lattice_planner(data, rasterizer)  # TODO: lattice planner path
-    position_preds_lp.append(result_lp)  # TODO: set to list of 50 positions
-    yaw_preds_lp.append(result_lp)  # TODO: set to list of 50 headings
-#     our_map, start_position, end_position, start_heading, end_heading = extract_map(data, rasterizer)
-#     start_x = start_position[0]
-#     start_y = start_position[1]
-#     start_theta = start_heading
-#     start_curvature = 0
-#     goal_x = end_position[0]
-#     goal_y = end_position[1]
-#     goal_theta = end_heading
-#     goal_curvature = 1
-#     pg = PathGenerator(start_x, start_y, start_theta, start_curvature,
-#         goal_x, goal_y, goal_theta, goal_curvature,
-#         alpha=10, beta=10, gamma=10, kmax=0.5)
-#     print(pg.path)
+    positions_lp, yaws_lp = lattice_planner(data, rasterizer)
+    position_preds_lp.append(positions_lp[np.newaxis, :])
+    yaw_preds_lp.append(yaws_lp[np.newaxis, :])
 
     # Ground truth
-    position_gts.append(data["target_positions"].detach().cpu().numpy())  # TODO: check shape consistencies if squeeze is required
-    yaw_gts.append(data["target_yaws"].detach().cpu().numpy())
-#     position_gts.append(data["target_positions"])
-#     yaw_gts.append(data["target_yaws"])
+    position_gts.append(data["target_positions"][np.newaxis, :])
+    yaw_gts.append(data["target_yaws"][np.newaxis, :])
 
-    if idx_data == 10:
+    if idx_data == 1:
         break
     idx_data += 1
 
 position_preds_dl = np.concatenate(position_preds_dl)
 yaw_preds_dl = np.concatenate(yaw_preds_dl)
-# TODO: any required manipulation of lattice planner result arrays
+position_preds_lp = np.concatenate(position_preds_lp)
+yaw_preds_lp = np.concatenate(yaw_preds_lp)
 position_gts = np.concatenate(position_gts)
 yaw_gts = np.concatenate(yaw_gts)
 
 # Quantitative evaluation
-pos_errors = np.linalg.norm(position_preds_dl - position_gts, axis=-1)
+pos_errors_dl = np.linalg.norm(position_preds_dl - position_gts, axis=-1)
+pos_errors_lp = np.linalg.norm(position_preds_lp - position_gts, axis=-1)
 # TODO: get errors between LP results and GT, and LP results and DL results
 # TODO: plot new set of results below
 
 # DISPLACEMENT AT T
-plt.plot(np.arange(pos_errors.shape[1]), pos_errors.mean(0), label="Displacement error at T")
+plt.plot(np.arange(pos_errors_dl.shape[1]), pos_errors_dl.mean(0), label="Displacement error at T")
 plt.legend()
 plt.show()
-
 # ADE HIST
-plt.hist(pos_errors.mean(-1), bins=100, label="ADE Histogram")
+plt.hist(pos_errors_dl.mean(-1), bins=100, label="ADE Histogram")
 plt.legend()
 plt.show()
 
 # FDE HIST
-plt.hist(pos_errors[:,-1], bins=100, label="FDE Histogram")
+plt.hist(pos_errors_dl[:, -1], bins=100, label="FDE Histogram")
 plt.legend()
 plt.show()
 
@@ -218,24 +210,27 @@ plt.legend()
 plt.show()
 
 # Qualitative evaluation
+LATTICE_POINTS_COLOR = (255, 0, 0)
 for frame_number in range(0, len(eval_dataset), len(eval_dataset) // 20):
     data = eval_dataloader.dataset[frame_number]
-
     data_batch = default_collate([data])
 
     result_dl = model(data_batch)
-    predicted_positions = result_dl["positions"].detach().cpu().numpy().squeeze()
+    predicted_positions_dl = result_dl["positions"].detach().cpu().numpy().squeeze()
+
+    predicted_positions_lp, _ = lattice_planner(data, rasterizer)
 
     im_ego = rasterizer.to_rgb(data["image"].transpose(1, 2, 0))
     target_positions = data["target_positions"]
 
-    predicted_positions = transform_points(predicted_positions, data["raster_from_agent"])
+    predicted_positions_dl = transform_points(predicted_positions_dl, data["raster_from_agent"])
+    predicted_positions_lp = transform_points(predicted_positions_lp, data["raster_from_agent"])
     target_positions = transform_points(target_positions, data["raster_from_agent"])
 
-    draw_trajectory(im_ego, predicted_positions, PREDICTED_POINTS_COLOR)
-
+    draw_trajectory(im_ego, predicted_positions_dl, PREDICTED_POINTS_COLOR)
+    draw_trajectory(im_ego, predicted_positions_lp, LATTICE_POINTS_COLOR)
     draw_trajectory(im_ego, target_positions, TARGET_POINTS_COLOR)
-
+    pdb.set_trace()
     plt.imshow(im_ego)
     plt.axis("off")
     plt.show()
@@ -251,14 +246,14 @@ for frame_number in range(200):
     data_batch = {k: v.to(device) for k, v in data_batch.items()}
 
     result_dl = model(data_batch)
-    predicted_positions = result_dl["positions"].detach().cpu().numpy().squeeze()
+    predicted_positions_dl = result_dl["positions"].detach().cpu().numpy().squeeze()
 
-    predicted_positions = transform_points(predicted_positions, data["raster_from_agent"])
+    predicted_positions_dl = transform_points(predicted_positions_dl, data["raster_from_agent"])
     target_positions = transform_points(data["target_positions"], data["raster_from_agent"])
 
     im_ego = rasterizer.to_rgb(data["image"].transpose(1, 2, 0))
     draw_trajectory(im_ego, target_positions, TARGET_POINTS_COLOR)
-    draw_trajectory(im_ego, predicted_positions, PREDICTED_POINTS_COLOR)
+    draw_trajectory(im_ego, predicted_positions_dl, PREDICTED_POINTS_COLOR)
 
     clear_output(wait=True)
     display(PIL.Image.fromarray(im_ego))
